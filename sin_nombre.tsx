d60d7938007904bbd442dfc0b28fd8272c4aa976
Óscar Hernández <oscarhernandezgomez@gmail.com>
2017-10-10 20:06:47 +0200

    yokese

---
+++ sin_nombre.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mapa Tileset" tilewidth="256" tileheight="256" spacing="1" margin="1" tilecount="28" columns="7">
+ <image source="png/Mapa Tileset.png" width="1800" height="1029"/>
+ <terraintypes>
+  <terrain name="BG" tile="0"/>
+  <terrain name="Tile" tile="0"/>
+  <terrain name="Acido" tile="0"/>
+  <terrain name="Tile 2" tile="0"/>
+  <terrain name="Nuevo Terreno" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,2,2"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,0"/>
+ <tile id="3" terrain=",,,1"/>
+ <tile id="4" terrain=",,1,1"/>
+ <tile id="5" terrain=",,1,"/>
+ <tile id="6" terrain=",1,1,1"/>
+ <tile id="7" terrain="2,2,2,2"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain=",1,,1"/>
+ <tile id="11" terrain="1,1,1,1"/>
+ <tile id="12" terrain="1,,1,"/>
+ <tile id="13" terrain="1,,1,1"/>
+ <tile id="14" terrain="0,0,,"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain=",1,,"/>
+ <tile id="18" terrain="1,1,,"/>
+ <tile id="19" terrain="1,,,"/>
+ <tile id="22" terrain=",,,4"/>
+ <tile id="23" terrain=",,4,4"/>
+ <tile id="24" terrain=",,4,"/>
+ <tile id="25" terrain=",3,,"/>
+ <tile id="26" terrain="3,3,,"/>
+ <tile id="27" terrain="3,,,"/>
+</tileset>
